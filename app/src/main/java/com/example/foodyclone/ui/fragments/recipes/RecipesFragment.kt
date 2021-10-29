11ca2007dee78d34af5c58e4282ad56fc60cf606
양태웅 <didxodnd0422@gmail.com>
2021-10-29 15:44:11 +0900

    Init Repo

---
+++ app/src/main/java/com/example/foodyclone/ui/fragments/recipes/RecipesFragment.kt
@@ -0,0 +1,79 @@
+package com.example.foodyclone.ui.fragments.recipes
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.foodyclone.viewmodels.MainViewModel
+import com.example.foodyclone.R
+import com.example.foodyclone.adapters.RecipesAdapter
+import com.example.foodyclone.databinding.FragmentRecipesBinding
+import com.example.foodyclone.util.NetworkResult
+import com.example.foodyclone.viewmodels.RecipesViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class RecipesFragment : Fragment() {
+    private lateinit var binding: FragmentRecipesBinding
+    private lateinit var mainViewModel: MainViewModel
+    private lateinit var recipesViewModel: RecipesViewModel
+    private val mAdapter by lazy { RecipesAdapter() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        recipesViewModel = ViewModelProvider(requireActivity()).get(RecipesViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_recipes, container, false)
+
+        setUpRecyclerView()
+        requestApiData()
+
+        return binding.root
+    }
+
+    private fun setUpRecyclerView() {
+        binding.recyclerview.adapter = mAdapter
+        binding.recyclerview.layoutManager = LinearLayoutManager(requireContext())
+
+        showShimmer()
+    }
+
+    private fun requestApiData() {
+        mainViewModel.getRecipes(recipesViewModel.applyQueries())
+        mainViewModel.recipeResponse.observe(viewLifecycleOwner, { response ->
+            when(response) {
+                is NetworkResult.Success -> {
+                    hideShimmer()
+                    response.data?.let { mAdapter.setData(it) }
+                }
+                is NetworkResult.Error -> {
+                    hideShimmer()
+                    Toast.makeText(requireContext(), response.message, Toast.LENGTH_SHORT).show()
+                }
+                is NetworkResult.Loading -> {
+                    showShimmer()
+                }
+            }
+        })
+    }
+
+    private fun showShimmer() {
+        binding.recyclerview.showShimmer()
+    }
+
+    private fun hideShimmer() {
+        binding.recyclerview.hideShimmer()
+    }
+}
---
+++ app/src/main/java/com/example/foodyclone/util/Constants.kt
@@ -0,0 +1,17 @@
+package com.example.foodyclone.util
+
+class Constants {
+    companion object {
+
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "fc491c9a9867484f8e228e340abe1ed7"
+
+        // API Query Keys
+        const val QUERY_NUMBER = "number"
+        const val QUERY_API_KEY = "apiKey"
+        const val QUERY_TYPE = "type"
+        const val QUERY_DIET = "diet"
+        const val QUERY_ADD_RECIPE_INFORMATION = "addRecipeInformation"
+        const val QUERY_FILL_INGREDIENTS = "fillIngredients"
+    }
+}
