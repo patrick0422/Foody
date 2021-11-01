d16dc8db661dd2c5fcfd96f1c8bd70d493a7d51b
양태웅 <didxodnd0422@gmail.com>
2021-11-01 23:01:07 +0900

    Entity 추가

--- app/src/main/java/com/example/foodyclone/RecipesEntity.kt
+++ app/src/main/java/com/example/foodyclone/RecipesEntity.kt
@@ -1,9 +1,14 @@
 package com.example.foodyclone
 
 import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.foodyclone.models.FoodRecipe
 import com.example.foodyclone.util.Constants.Companion.RECIPES_TABLE
 
 @Entity(tableName = RECIPES_TABLE)
-class RecipesEntity {
-
+class RecipesEntity(
+    var foodRecipe: FoodRecipe
+) {
+    @PrimaryKey(autoGenerate = false)
+    var id: Int = 0
 }
