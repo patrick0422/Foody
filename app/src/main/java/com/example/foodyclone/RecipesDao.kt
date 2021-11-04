fbc8539ba0150d45aed808853dfafa4ec1480b7f
양태웅 <didxodnd0422@gmail.com>
2021-11-04 11:37:59 +0900

    DAO 추가

---
+++ app/src/main/java/com/example/foodyclone/RecipesDao.kt
@@ -0,0 +1,16 @@
+package com.example.foodyclone
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface RecipesDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRecipes(recipesEntity: RecipesEntity)
+
+    @Query("SELECT * FROM recipes_table")
+    suspend fun readRecipes(): Flow<List<RecipesEntity>>
+}
