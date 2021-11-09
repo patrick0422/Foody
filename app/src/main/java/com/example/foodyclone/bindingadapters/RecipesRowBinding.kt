376cfc6117371dfa6d1b7805bf64cecb5f7b58f8
양태웅 <didxodnd0422@gmail.com>
2021-11-09 09:54:12 +0900

    BindingAdapter object로 변경

--- app/src/main/java/com/example/foodyclone/bindingadapters/RecipesRowBinding.kt
+++ app/src/main/java/com/example/foodyclone/bindingadapters/RecipesRowBinding.kt
@@ -8,47 +8,45 @@ import androidx.databinding.BindingAdapter
 import coil.load
 import com.example.foodyclone.R
 
-class RecipesRowBinding {
-    companion object {
-        @JvmStatic
-        @BindingAdapter("setNumberOfLikes")
-        fun setNumberOfLikes(textView: TextView, likes: Int) {
-            textView.text = likes.toString()
-        }
+object RecipesRowBinding {
+    @JvmStatic
+    @BindingAdapter("setNumberOfLikes")
+    fun setNumberOfLikes(textView: TextView, likes: Int) {
+        textView.text = likes.toString()
+    }
 
-        @JvmStatic
-        @BindingAdapter("setNumberOfMinutes")
-        fun setNumberOfMinutes(textView: TextView, minutes: Int) {
-            textView.text = minutes.toString()
-        }
+    @JvmStatic
+    @BindingAdapter("setNumberOfMinutes")
+    fun setNumberOfMinutes(textView: TextView, minutes: Int) {
+        textView.text = minutes.toString()
+    }
 
-        @JvmStatic
-        @BindingAdapter("applyVeganColor")
-        fun applyVeganColor(view: View, vegan: Boolean) {
-            if (vegan) {
-                when (view) {
-                    is TextView -> view.setTextColor(
-                        ContextCompat.getColor(
-                            view.context,
-                            R.color.green
-                        )
+    @JvmStatic
+    @BindingAdapter("applyVeganColor")
+    fun applyVeganColor(view: View, vegan: Boolean) {
+        if (vegan) {
+            when (view) {
+                is TextView -> view.setTextColor(
+                    ContextCompat.getColor(
+                        view.context,
+                        R.color.green
                     )
-                    is ImageView -> view.setColorFilter(
-                        ContextCompat.getColor(
-                            view.context,
-                            R.color.green
-                        )
+                )
+                is ImageView -> view.setColorFilter(
+                    ContextCompat.getColor(
+                        view.context,
+                        R.color.green
                     )
-                }
+                )
             }
         }
+    }
 
-        @JvmStatic
-        @BindingAdapter("loadImageFromUrl")
-        fun loadImageFromUrl(imageView: ImageView, imageUrl: String) {
-            imageView.load(imageUrl) {
-                crossfade(600)
-            }
+    @JvmStatic
+    @BindingAdapter("loadImageFromUrl")
+    fun loadImageFromUrl(imageView: ImageView, imageUrl: String) {
+        imageView.load(imageUrl) {
+            crossfade(600)
         }
     }
 }
