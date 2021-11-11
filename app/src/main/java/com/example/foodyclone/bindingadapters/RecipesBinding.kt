98c77ae4ae2697859c30a34bbb6c80fd1aa011b9
양태웅 <didxodnd0422@gmail.com>
2021-11-11 20:27:43 +0900

    RecipesBinding 추가

---
+++ app/src/main/java/com/example/foodyclone/bindingadapters/RecipesBinding.kt
@@ -0,0 +1,41 @@
+package com.example.foodyclone.bindingadapters
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.example.foodyclone.data.database.RecipesEntity
+import com.example.foodyclone.models.FoodRecipe
+import com.example.foodyclone.util.NetworkResult
+
+object RecipesBinding {
+
+    @JvmStatic
+    @BindingAdapter("readApiResponse", "readDatabase", requireAll = true)
+    fun errorImageViewVisibility(
+        view: ImageView,
+        apiResponse: NetworkResult<FoodRecipe?>?,
+        database: List<RecipesEntity>?
+    ) {
+        if (apiResponse is NetworkResult.Error && database.isNullOrEmpty()) {
+            view.visibility = View.VISIBLE
+        } else {
+            view.visibility = View.INVISIBLE
+        }
+    }
+
+    @JvmStatic
+    @BindingAdapter("readApiResponse2", "readDatabase2", requireAll = true)
+    fun errorTextViewVisibility(
+        view: TextView,
+        apiResponse: NetworkResult<FoodRecipe?>?,
+        database: List<RecipesEntity>?
+    ) {
+        if (apiResponse is NetworkResult.Error && database.isNullOrEmpty()) {
+            view.visibility = View.VISIBLE
+            view.text = apiResponse.message
+        } else {
+            view.visibility = View.INVISIBLE
+        }
+    }
+}
