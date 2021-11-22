ed5072988458a98a5a500d1859785b74454667c6
양태웅 <didxodnd0422@gmail.com>
2021-11-22 11:57:01 +0900

    DataStoreRepository에 읽기/쓰기 함수 추가

--- app/src/main/java/com/example/foodyclone/data/DataStoreRepository.kt
+++ app/src/main/java/com/example/foodyclone/data/DataStoreRepository.kt
@@ -3,15 +3,18 @@ package com.example.foodyclone.data
 import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.dataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.intPreferencesKey
-import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.core.*
 import androidx.datastore.preferences.preferencesDataStore
 import com.example.foodyclone.util.Constants
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.android.scopes.ActivityRetainedScoped
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.map
+import java.io.IOException
 import javax.inject.Inject
 
+@ActivityRetainedScoped
 class DataStoreRepository @Inject constructor(@ApplicationContext private val context: Context) {
     private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = Constants.PREFERENCES_NAME)
 
@@ -23,7 +26,12 @@ class DataStoreRepository @Inject constructor(@ApplicationContext private val co
         val selectedDietTypeId = intPreferencesKey(Constants.PREFERENCES_DIET_TYPE_ID)
     }
 
-    fun saveMealAndDietType(mealType: String, mealTypeId: Int, dietType: String, dietTypeId: Int) {
+    suspend fun saveMealAndDietType(
+        mealType: String,
+        mealTypeId: Int,
+        dietType: String,
+        dietTypeId: Int
+    ) {
         context.dataStore.edit { preferences ->
             preferences[PreferenceKeys.selectedMealType] = mealType
             preferences[PreferenceKeys.selectedMealTypeId] = mealTypeId
@@ -31,6 +39,22 @@ class DataStoreRepository @Inject constructor(@ApplicationContext private val co
             preferences[PreferenceKeys.selectedDietTypeId] = dietTypeId
         }
     }
+
+    val readMealAndDietType: Flow<MealAndDietType> = context.dataStore.data
+        .catch { exception ->
+            if (exception is IOException)
+                emit(emptyPreferences())
+            else
+                throw exception
+        }
+        .map { preferences ->
+            MealAndDietType(
+                preferences[PreferenceKeys.selectedMealType] ?: Constants.DEFAULT_MEAL_TYPE,
+                preferences[PreferenceKeys.selectedMealTypeId] ?: 0,
+                preferences[PreferenceKeys.selectedDietType] ?: Constants.DEFAULT_DIET_TYPE,
+                preferences[PreferenceKeys.selectedDietTypeId] ?: 0,
+            )
+        }
 }
 
 data class MealAndDietType(
