abe2e1ab418d59a3265cb07a70a14d9ab31dec87
양태웅 <didxodnd0422@gmail.com>
2021-11-22 09:41:05 +0900

    DataStoreRepository 추가

--- app/src/main/java/com/example/foodyclone/data/DataStoreRepository.kt
+++ app/src/main/java/com/example/foodyclone/data/DataStoreRepository.kt
@@ -1,4 +1,41 @@
 package com.example.foodyclone.data
 
-class DataStoreRepository {
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.dataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.intPreferencesKey
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import com.example.foodyclone.util.Constants
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+
+class DataStoreRepository @Inject constructor(@ApplicationContext private val context: Context) {
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = Constants.PREFERENCES_NAME)
+
+    private object PreferenceKeys {
+        val selectedMealType = stringPreferencesKey(Constants.PREFERENCES_MEAL_TYPE)
+        val selectedMealTypeId = intPreferencesKey(Constants.PREFERENCES_MEAL_TYPE_ID)
+
+        val selectedDietType = stringPreferencesKey(Constants.PREFERENCES_DIET_TYPE)
+        val selectedDietTypeId = intPreferencesKey(Constants.PREFERENCES_DIET_TYPE_ID)
+    }
+
+    fun saveMealAndDietType(mealType: String, mealTypeId: Int, dietType: String, dietTypeId: Int) {
+        context.dataStore.edit { preferences ->
+            preferences[PreferenceKeys.selectedMealType] = mealType
+            preferences[PreferenceKeys.selectedMealTypeId] = mealTypeId
+            preferences[PreferenceKeys.selectedDietType] = dietType
+            preferences[PreferenceKeys.selectedDietTypeId] = dietTypeId
+        }
+    }
 }
+
+data class MealAndDietType(
+    val selectedMealType: String,
+    val selectedMealTypeId: Int,
+    val selectedDietType: String,
+    val selectedDietTypeId: Int
+)
