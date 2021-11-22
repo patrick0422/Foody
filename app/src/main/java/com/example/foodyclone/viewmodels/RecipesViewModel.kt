cff5d93131d8f0035e3b80e8acf1e0c435decd78
양태웅 <didxodnd0422@gmail.com>
2021-11-22 11:55:49 +0900

    DataStore에 대한 참조, 읽기/쓰기 함수 추가,

--- app/src/main/java/com/example/foodyclone/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/example/foodyclone/viewmodels/RecipesViewModel.kt
@@ -2,6 +2,8 @@ package com.example.foodyclone.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.foodyclone.data.DataStoreRepository
 import com.example.foodyclone.util.Constants.Companion.API_KEY
 import com.example.foodyclone.util.Constants.Companion.DEFAULT_DIET_TYPE
 import com.example.foodyclone.util.Constants.Companion.DEFAULT_MEAL_TYPE
@@ -12,16 +14,50 @@ import com.example.foodyclone.util.Constants.Companion.QUERY_DIET
 import com.example.foodyclone.util.Constants.Companion.QUERY_FILL_INGREDIENTS
 import com.example.foodyclone.util.Constants.Companion.QUERY_NUMBER
 import com.example.foodyclone.util.Constants.Companion.QUERY_TYPE
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class RecipesViewModel(application: Application): AndroidViewModel(application) {
+class RecipesViewModel @Inject constructor(
+    application: Application,
+    private val dataStoreRepository: DataStoreRepository
+) : AndroidViewModel(application) {
+
+    private var mealType = DEFAULT_MEAL_TYPE
+    private var dietType = DEFAULT_DIET_TYPE
+
+    val readMealAndDietType = dataStoreRepository.readMealAndDietType
+
+    fun saveMealAndDietType(
+        selectedMealType: String,
+        selectedMealTypeId: Int,
+        selectedDietType: String,
+        selectedDietTypeId: Int
+    ) = viewModelScope.launch(Dispatchers.IO) {
+        dataStoreRepository.saveMealAndDietType(
+            selectedMealType,
+            selectedMealTypeId,
+            selectedDietType,
+            selectedDietTypeId
+        )
+    }
 
     fun applyQueries(): HashMap<String, String> {
         val queries: HashMap<String, String> = HashMap()
 
+        // Flow는 collect를 통해 값을 받아옴
+        viewModelScope.launch(Dispatchers.IO) {
+            readMealAndDietType.collect { value ->
+                mealType = value.selectedMealType
+                dietType = value.selectedDietType
+            }
+        }
+
         queries[QUERY_NUMBER] = DEFAULT_RECIPES_NUMBER
         queries[QUERY_API_KEY] = API_KEY
-        queries[QUERY_TYPE] = DEFAULT_MEAL_TYPE
-        queries[QUERY_DIET] = DEFAULT_DIET_TYPE
+        queries[QUERY_TYPE] = mealType
+        queries[QUERY_DIET] = dietType
         queries[QUERY_ADD_RECIPE_INFORMATION] = "true"
         queries[QUERY_FILL_INGREDIENTS] = "true"
 
