1f9d628cd16e9d609d0523d05aaf6ec4c67495ff
양태웅 <didxodnd0422@gmail.com>
2022-03-16 20:55:02 +0900

    Add Toolbar, TabLayout, ViewPager2 in DetailsActivity

--- app/src/main/java/com/example/foodyclone/ui/DetailsActivity.kt
+++ app/src/main/java/com/example/foodyclone/ui/DetailsActivity.kt
@@ -2,11 +2,28 @@ package com.example.foodyclone.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.foodyclone.R
+import com.example.foodyclone.databinding.ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private val binding: ActivityDetailsBinding by lazy { DataBindingUtil.setContentView(this, R.layout.activity_details) }
+
+    override fun onCreate(savedInstanceState: Bundle?): Unit = with(binding) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+
+        setSupportActionBar(toolbar)
+        toolbar.setTitleTextColor(getColor(R.color.white))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
