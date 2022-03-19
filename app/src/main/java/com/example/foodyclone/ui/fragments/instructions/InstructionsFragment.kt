2973fdcef4487d661f34ba2da30c5bfca01b3b9f
양태웅 <didxodnd0422@gmail.com>
2022-03-19 19:04:55 +0900

    Implement InstructionsFragment

--- app/src/main/java/com/example/foodyclone/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/example/foodyclone/ui/fragments/instructions/InstructionsFragment.kt
@@ -5,9 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import androidx.databinding.DataBindingUtil
 import com.example.foodyclone.R
 import com.example.foodyclone.databinding.FragmentInstructionsBinding
+import com.example.foodyclone.models.Result
+import com.example.foodyclone.util.Constants
 
 class InstructionsFragment : Fragment() {
     private lateinit var binding: FragmentInstructionsBinding
@@ -18,7 +21,13 @@ class InstructionsFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_instructions, container, false)
 
+        val args = arguments
+        val result: Result? = args?.getParcelable(Constants.BUNDLE_RECIPE_KEY)
 
+        binding.instructionsWebView.webViewClient = object : WebViewClient() {
+
+        }
+        binding.instructionsWebView.loadUrl(result?.sourceUrl ?: "")
 
         return binding.root
     }
