eeff30dccbff3e30f1d0384203f4f315d6767450
양태웅 <didxodnd0422@gmail.com>
2022-03-19 17:20:00 +0900

    Generify RecipesDiffUtil

--- app/src/main/java/com/example/foodyclone/util/RecipesDiffUtil.kt
+++ app/src/main/java/com/example/foodyclone/util/RecipesDiffUtil.kt
@@ -3,9 +3,9 @@ package com.example.foodyclone.util
 import androidx.recyclerview.widget.DiffUtil
 import com.example.foodyclone.models.Result
 
-class RecipesDiffUtil(
-    private val oldList: List<Result>,
-    private val newList: List<Result>
+class RecipesDiffUtil<T>(
+    private val oldList: List<T>,
+    private val newList: List<T>
 ): DiffUtil.Callback() {
     override fun getOldListSize(): Int = oldList.size
 
