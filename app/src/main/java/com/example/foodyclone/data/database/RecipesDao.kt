81af844843a48f2436edc60a105ff3e2e5c4e741
양태웅 <didxodnd0422@gmail.com>
2022-03-21 16:03:32 +0900

    Edit RecipesDao

--- app/src/main/java/com/example/foodyclone/data/database/RecipesDao.kt
+++ app/src/main/java/com/example/foodyclone/data/database/RecipesDao.kt
@@ -1,10 +1,9 @@
 package com.example.foodyclone.data.database
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
+import com.example.foodyclone.data.database.entities.FavoritesEntity
 import com.example.foodyclone.data.database.entities.RecipesEntity
+import com.example.foodyclone.models.Result
 import kotlinx.coroutines.flow.Flow
 
 @Dao
@@ -14,4 +13,17 @@ interface RecipesDao {
 
     @Query("SELECT * FROM recipes_table")
     fun readRecipes(): Flow<List<RecipesEntity>>
+
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavoriteRecipes(favoritesEntity: FavoritesEntity)
+
+    @Query("SELECT * FROM favorite_recipes_table ORDER BY id ASC")
+    fun readFavorites(): Flow<List<FavoritesEntity>>
+
+    @Delete
+    suspend fun deleteFavoriteRecipes(favoritesEntity: FavoritesEntity)
+
+    @Query("DELETE FROM favorite_recipes_table")
+    suspend fun deleteAllFavoriteRecipes()
 }
