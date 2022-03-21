bd11f031b9737079cfcf277748697e9dc3aabbe3
양태웅 <didxodnd0422@gmail.com>
2022-03-21 16:03:40 +0900

    Edit LocalDataSource

--- app/src/main/java/com/example/foodyclone/data/LocalDataSource.kt
+++ app/src/main/java/com/example/foodyclone/data/LocalDataSource.kt
@@ -1,6 +1,7 @@
 package com.example.foodyclone.data
 
 import com.example.foodyclone.data.database.RecipesDao
+import com.example.foodyclone.data.database.entities.FavoritesEntity
 import com.example.foodyclone.data.database.entities.RecipesEntity
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -9,9 +10,17 @@ class LocalDataSource @Inject constructor(
     private val recipesDao: RecipesDao
 ) {
 
-    suspend fun insertRecipes(recipesEntity: RecipesEntity) {
-        recipesDao.insertRecipes(recipesEntity)
-    }
+    suspend fun insertRecipes(recipesEntity: RecipesEntity) = recipesDao.insertRecipes(recipesEntity)
 
     fun readRecipes(): Flow<List<RecipesEntity>> = recipesDao.readRecipes()
+
+
+    suspend fun insertFavoriteRecipes(favoritesEntity: FavoritesEntity) = recipesDao.insertFavoriteRecipes(favoritesEntity)
+
+    fun readFavoriteRecipes(): Flow<List<FavoritesEntity>> = recipesDao.readFavorites()
+
+    suspend fun deleteFavoriteRecipes(favoritesEntity: FavoritesEntity) = recipesDao.deleteFavoriteRecipes(favoritesEntity)
+
+    suspend fun deleteAllFavoriteRecipes() = recipesDao.deleteAllFavoriteRecipes()
+
 }
