b838beb210ad18adac8b5209f88137d23df532d5
양태웅 <didxodnd0422@gmail.com>
2022-03-23 15:32:43 +0900

    Add FavoriteRecipes BindingAdapter

--- app/src/main/java/com/example/foodyclone/bindingadapters/FavoriteRecipesBinding.kt
+++ app/src/main/java/com/example/foodyclone/bindingadapters/FavoriteRecipesBinding.kt
@@ -1,4 +1,35 @@
 package com.example.foodyclone.bindingadapters
 
+import android.view.View
+import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.foodyclone.adapters.FavoritesAdapter
+import com.example.foodyclone.data.database.entities.FavoritesEntity
+
 object FavoriteRecipesBinding {
+    @JvmStatic
+    @BindingAdapter("viewVisibility", "setData", requireAll = false)
+    fun setDataAndViewVisibility(view: View, favoritesEntity: List<FavoritesEntity>?, adapter: FavoritesAdapter?) {
+        if (favoritesEntity.isNullOrEmpty()) {
+            when(view) {
+                is RecyclerView -> {
+                    view.visibility = View.INVISIBLE
+                }
+                else -> {
+                    view.visibility = View.VISIBLE
+                }
+            }
+        } else {
+            when(view) {
+                is RecyclerView -> {
+                    view.visibility = View.VISIBLE
+                    adapter?.setData(favoritesEntity)
+                }
+                else -> {
+                    view.visibility = View.INVISIBLE
+                }
+            }
+        }
+
+    }
 }
